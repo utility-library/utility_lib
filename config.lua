60af0efad4969a6de725bf78e442df1a52ef9261
Xenos.exe <55803068+XenoS-ITA@users.noreply.github.com>
2021-07-09 10:04:18 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,32 +2,8 @@ Config = {}
 
 Config.SecondJob = "" -- If you have a second job insert it here
 
--- Marker
-Config.UpdateCooldown = 500 -- (default: 500ms) This is the time when the position/distance of any marker will update
+-- Marker/Object
+Config.UpdateCooldown = 500 -- (default: 500ms) This is the time when the position/distance will update
 
-
-
-
-
-
-
-
-
-
-------------------------------------------------------------------------------------------------------------------------------
--- THIS IS ONLY THE TRIGGER, IF YOU DISABLE IT YOU CAN STILL USE THE NATIVE BUT DONT CHANGE/GIVE ANYTHING (DONT GIVE ERROR) --
-------------------------------------------------------------------------------------------------------------------------------
-
--- Implement GetDataFromDb, SaveDataToDb and UpdateDataToDb native (the db table can be only the default)
-Config.DB_integration = false
-
--- Implement AddItem, GetItem, RemoveItem, AddMoney and RemoveMoney native/trigger
-Config.ESX_integration = {
-    main_switch = false, -- This is the main switch, if it is disabled all the trigger below will be disabled, otherwise only those you have activated will be functional
-
-    AddItem     = true,
-    GetItem     = true,
-    RemoveItem  = true,
-    AddMoney    = true,
-    RemoveMoney = true
-}
+-- Dialogue
+Config.UpdateDialogue = 100 -- (default: 100ms) This is the time when the 3dText will update
