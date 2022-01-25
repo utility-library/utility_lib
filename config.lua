dddb6d080524fb553a32b93f1eeeb065ac768ef3
Xenos.exe <55803068+XenoS-ITA@users.noreply.github.com>
2022-01-25 12:11:34 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,5 @@
 Config = {}
 
-Config.SecondJob = "" -- If you have a second job insert it here
-
 -- Marker/Object
 Config.UpdateCooldown = 500 -- (default: 500ms) This is the time when the position/distance will update
 
