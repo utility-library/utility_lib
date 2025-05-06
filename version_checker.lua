a2045ce44559705edbc413aeb611ed2fd87ed8ab
XenoS-ITA <arcobalenosixsiegea@gmail.com>
2025-05-06 18:26:15 +0200

    update version

--- version_checker.lua
+++ version_checker.lua
@@ -1,4 +1,4 @@
-local version = '1.3.0'
+local version = '1.3.1'
 local versionurl = "https://raw.githubusercontent.com/utility-library/utility_lib/master/version"
 
 PerformHttpRequest(versionurl, function(error, _version, header)
